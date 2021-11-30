7b1856c16cfd801d11aa47830a04d15f7bb176a6
Nexus20 <chubarov.zheka@gmail.com>
2021-12-01 00:44:56 +0200

    Added annotations to the create car view model

--- CourseProject.WEB/Models/CreateEditCarViewModel.cs
+++ CourseProject.WEB/Models/CreateEditCarViewModel.cs
@@ -1,8 +1,13 @@
-﻿namespace CourseProject.WEB.Models; 
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CourseProject.WEB.Models; 
 
 public class CreateEditCarViewModel : BaseViewModel {
+    
     public string Submodel { get; set; }
 
+    [Required]
+    [Display(Name = "Model")]
     public int ModelId { get; set; }
 
     public IFormFileCollection Images { get; set; }
